b4e7c98289bd8cdc71f4eb19f91ad8dda1d326b9
James <demonsythe.jm@gmail.com>
2023-09-26 17:56:20 +1000

    Update Map Marker.c

--- other/Map Marker.c
+++ other/Map Marker.c
@@ -1,18 +1,34 @@
-	#ifdef EXPANSIONMODNAVIGATION
+
+//in class
+#ifdef EXPANSIONMODNAVIGATION
    //declares
 	ExpansionMarkerModule m_MarkerModule;
 	ExpansionMarkerData m_ServerMarker;
-	//create
-		if (CF_Modules<ExpansionMarkerModule>.Get(m_MarkerModule))
-		m_ServerMarker = m_MarkerModule.CreateServerMarker("AI Spawn", "Territory", ai.GetPosition(), ARGB(255, 235, 59, 90), true);
-	//remove
-	GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(removeMissionMarker), 15000, false, m_ServerMarker.GetUID();
 
-	
+    
+    void CreateMissionMarker(vector pos, string name, int timer, int remove = 0)
+    {
+    if (CF_Modules<ExpansionMarkerModule>.Get(m_MarkerModule))
+	{
+        m_ServerMarker = m_MarkerModule.CreateServerMarker(name, "Territory", pos, ARGB(255, 235, 59, 90), true);
+        if (remove == 1)
+        GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(RemoveMissionMarker, timer, false, m_ServerMarker.GetUID());
+    }
+
+    }
+
 	void RemoveMissionMarker(string uid)
 	{
 		if ( !m_ServerMarker )
 		return;
 		m_MarkerModule.RemoveServerMarker( uid );
 	}
-	#endif
+#endif
+
+
+//to use in class
+
+  #ifdef EXPANSIONMODNAVIGATION
+        int Remove = 1;
+        CreateMissionMarker("10 5 10", "Map Marker", 50000, Remove);
+  #endif
